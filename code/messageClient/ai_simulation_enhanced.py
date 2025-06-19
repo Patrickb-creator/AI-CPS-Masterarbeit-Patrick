9f376014c9e0b7d3c9b7f2b2d05a7cd67db67dc7
Patrick_Beyer <beyer701@googlemail.com>
2025-06-19 13:56:20 +0200

    Author

--- code/messageClient/ai_simulation_enhanced.py
+++ code/messageClient/ai_simulation_enhanced.py
@@ -7,7 +7,7 @@ Copyright (c) 2022 Marcus Grum
 __author__ = 'Marcus Grum, marcus.grum@uni-potsdam.de'
 
 # with friendly permissions by Marcus Grum:
-__thesis_author__ = 'Lena Siegmund, siegmund2@uni-potsdam.de'
+__thesis_author__ = 'Patrick Beyer, pbeyer@uni-potsdam.de'
 
 # SPDX-License-Identifier: AGPL-3.0-or-later or individual license
 # SPDX-FileCopyrightText: 2022 Marcus Grum <marcus.grum@uni-potsdam.de>
